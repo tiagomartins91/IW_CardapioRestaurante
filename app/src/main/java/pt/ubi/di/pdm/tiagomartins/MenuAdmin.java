e2c8f947f5f2ac4926f6fe5775b836e8dcdfe984
TiagoMartins <tiago_alex_martins@hotmail.com>
2017-11-14 14:14:37 +0000

    Initial commit

---
+++ app/src/main/java/pt/ubi/di/pdm/tiagomartins/MenuAdmin.java
@@ -0,0 +1,21 @@
+package pt.ubi.di.pdm.tiagomartins;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by TiagoMartins on 11/11/2017.
+ */
+
+public class MenuAdmin extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menuadmin);
+    }
+
+
+
+}
---
+++ app/src/main/java/pt/ubi/di/pdm/tiagomartins/MenuPrincipal.java
@@ -0,0 +1,33 @@
+package pt.ubi.di.pdm.tiagomartins;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MenuPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_principal);
+    }
+
+
+
+
+    public void startMenuAdmin(View v){
+
+
+        Intent startMenuAdmin = new Intent(this , MenuAdmin.class);
+
+        startActivity(startMenuAdmin);
+
+
+    }
+
+
+
+
+
+}
