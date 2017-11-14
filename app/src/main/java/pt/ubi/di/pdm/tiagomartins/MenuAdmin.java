3e390f43184400e868ad19193194ff27b0d3aeae
TiagoMartins <tiago_alex_martins@hotmail.com>
2017-11-14 15:23:20 +0000

    Programar a BD

--- app/src/main/java/pt/ubi/di/pdm/tiagomartins/MenuAdmin.java
+++ app/src/main/java/pt/ubi/di/pdm/tiagomartins/MenuAdmin.java
@@ -1,7 +1,9 @@
 package pt.ubi.di.pdm.tiagomartins;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by TiagoMartins on 11/11/2017.
@@ -14,6 +16,17 @@ public class MenuAdmin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menuadmin);
+    }
+
+
+    public void startAdicionarPrato(View v){
+
+
+        Intent startAdicionarPrato = new Intent(this , AdicionarPrato.class);
+
+        startActivity(startAdicionarPrato);
+
+
     }
 
 
